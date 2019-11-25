3be8ffcacc65f9b43f477a2f1616185d529ef6cb
Alexis Dumortier <dumortier.alexis@gmail.com>
2019-11-25 16:03:10 -0700

    Add supply to Node and to relevant methods

--- lib/wagon_train.rb
+++ lib/wagon_train.rb
@@ -9,12 +9,27 @@ class WagonTrain
     @list = LinkedList.new
   end
 
-  def append(name)
-    self.list.append(name)
+  def append(name, supply)
+    self.list.append(name, supply)
   end
 
   def count
     self.list.count
+  end
+
+  def supplies
+    temp_node = self.list.head
+    total_supply = Hash.new(0)
+    while temp_node.next_node != nil
+      temp_node.supplies.keys.each do |key|
+      total_supply[key] += temp_node.supplies[key]
+      end
+      temp_node = temp_node.next_node
+    end
+    temp_node.supplies.keys.each do |key|
+    total_supply[key] += temp_node.supplies[key]
+    end
+    total_supply
   end
 
 end
